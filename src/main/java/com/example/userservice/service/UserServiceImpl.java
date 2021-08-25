da65c2a62d06a7d308524b979c7007f081986343
huidragon-deali <79898452+huidragon-deali@users.noreply.github.com>
2021-08-25 20:11:53 +0900

    .

--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,8 +6,9 @@ import com.example.userservice.repository.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -60,7 +61,26 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public UserDto getUserByEmail(String email) {
+        UserEntity userEntity = userRepository.findByEmail(email);
+        return new ModelMapper().map(userEntity, UserDto.class);
+    }
+
+    @Override
     public Iterable<UserEntity> getUserByAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username);
+
+        if (userEntity == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(),
+                true , true, true, true,
+                new ArrayList<>());
     }
 }
--- src/main/java/com/example/userservice/vo/RequestLogin.java
+++ src/main/java/com/example/userservice/vo/RequestLogin.java
@@ -2,21 +2,19 @@ package com.example.userservice.vo;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
-public class RequestUser {
+public class RequestLogin {
 
     @NotNull(message = "email cannot null be null")
     @Size(min = 2)
+    @Email
     String email;
-
-    @NotNull(message = "name cannot null be null")
-    @Size(min = 2)
-    String name;
 
     @NotNull(message = "pwd cannot null be null")
     @Size(min = 8)
-    String pwd;
+    String password;
 }
